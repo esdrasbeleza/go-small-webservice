93c9372e5990209d5dbc4a53d2c0197a58958be5
Esdras Beleza <esdras@atlantico.com.br>
2015-01-20 18:17:10 -0300

    Using testify

--- note_test.go
+++ note_test.go
@@ -1,13 +1,24 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestEmptyNote(t *testing.T) {
 	note := NewEmptyNote()
 
-	if len(note.Title) > 0 {
-		t.Error("Text should be empty")
-	} else if len(note.Text) > 0 {
-		t.Error("Text should be empty")
-	}
+	assert.Equal(t, len(note.Title), 0, "Title should be empty")
+	assert.Equal(t, len(note.Text), 0, "Text should be empty")
+	assert.NotNil(t, note.Id)
+}
+
+func TestNewNote(t *testing.T) {
+	title := "My title"
+	text := "My text!"
+	note := NewNote(title, text)
+
+	assert.Equal(t, title, note.Title, "Title is wrong")
+	assert.Equal(t, text, note.Text, "Text is wrong")
+	assert.NotNil(t, note.Id)
 }
